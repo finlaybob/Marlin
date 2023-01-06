3c98a7be2919de4992261c4cb5bc1666f509c310
Neil <finbsp@gmail.com>
2023-01-06 17:57:40 +0000

    updates

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2858,7 +2858,7 @@
   #endif
 
   #if AXIS_IS_TMC_CONFIG(E0)
-    #define E0_CURRENT      580
+    #define E0_CURRENT      650
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
